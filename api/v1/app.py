d7e50f27a28e36e2cf196d76b02c70dc1bb5d4d1
Victor Amajuoyi <victorgavon@gmail.com>
2024-04-29 11:51:01 +0100

    AirBnB_clone_v3 - test bug fixes

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Createnew Flask app; and reg blueprint app_views to the Flask instance app.
+"""
+# Import necessary modules
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
+from models import storage  # Imports the storage instance from models package
+from api.v1.views import app_views  # Import app_views BP from api.v1.views
+
+
+# Create a Flask app instance
+app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+# Register the app_views blueprint to the Flask instance app
+app.register_blueprint(app_views)
+
+
+# Main entry point of the application
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
