a83dc6b2b5861ba5b5a5ff26b4c2c83fccf9f4b4
Victor Amajuoyi <victorgavon@gmail.com>
2024-04-29 12:07:27 +0100

    AirBnB_clone_v3 - app.py update

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,29 +16,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-    """
-    storage.close()
-
-
-@app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
-
 # Main entry point of the application
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
