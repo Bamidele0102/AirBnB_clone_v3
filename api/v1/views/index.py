3645d500a5569af37e71c4aa5fe2fcf5a1d5fd15
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-29 17:51:21 +0100

    Some stats? Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python3
-"""Create a flask app that starts an API"""
+"""
+index
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
+
 from models import storage
 
 
-# Create a route /status on the object app_views that returns JSON
-@app_views.route("/status")
-def api_status():
-    """A route that returns status OK"""
-    response = {"status": "OK"}
-    return jsonify(response)
-
-
-@app_views.route("/stats")
-def get_stats():
-    """A route that returns status OK"""
-    stats = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User"),
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
     }
-    return jsonify(stats)
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
