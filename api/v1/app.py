37d8c707ccf3de00fe8d6694cd68101a330e3194
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-29 14:27:14 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 """
-Createnew Flask app; and reg blueprint app_views to the Flask instance app.
+Creating a Flask app; and registering blueprint (app_views)
+to the Flask instance app.
 """
 # Import necessary modules
-from os import getenv
-from flask import Flask, jsonify
-from models import storage  # Imports the storage instance from models package
+from flask import Flask
 from api.v1.views import app_views  # Import app_views BP from api.v1.views
+from models import storage  # Imports the storage instance from models package
+import os
 
 
-# Create a Flask app instance
+# Set up the Flask app
 app = Flask(__name__)
 
 # Register the app_views blueprint to the Flask instance app
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
-# Main entry point of the application
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Close storage session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Createnew Flask app; app_views
+Creates a route /status on the object app_views
+that returns a JSON: "status": "OK"
 """
+
 from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def api_status():
-    """
-
-    """
-    response = {'status': "OK"}
-    return jsonify(response)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """Returns API status."""
+    return jsonify({"status": "OK"})
